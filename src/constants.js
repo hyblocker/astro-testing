740a99c8bb85ecbed5c931f275f9c020b01a4e27
Hyblocker <bryan.spiteri@gmail.com>
2021-10-16 23:42:54 +0200

    i18n i think

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,2 @@
 export const SITE_NAME = "Astro Test";
 export const DOMAIN = "astro-testing.vercel.app";
-export let DOCS_TREE = {};
-export function addDoc(doc) {
-    if (DOCS_TREE === undefined)
-        DOCS_TREE = {};
-    if (DOCS_TREE[doc.lang] === undefined)
-        DOCS_TREE[doc.lang] = [];
-    
-    DOCS_TREE[doc.lang].push({
-        text: doc.displayTitle,
-        url: doc.path,
-    });
-    
-    // unique values by url
-    DOCS_TREE[doc.lang] = [...new Map(DOCS_TREE[doc.lang].map((item) => [item["url"], item])).values()];
-}
